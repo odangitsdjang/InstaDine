dd80a72c39c780cfe11d7d817b268f853d3d5f70
addriv <adrivero89@gmail.com>
2017-10-10 19:21:10 -0700

    Start switch to DrawerNavigator

--- frontend/components/Splash.js
+++ frontend/components/Splash.js
@@ -72,7 +72,7 @@ const styles = StyleSheet.create({
     flexDirection: 'column'
   },
   title: {
-    fontSize: 30,
+    fontSize: 40,
     padding: 30
   },
   authButtons: {
--- frontend/navigators/HomeNavigator.js
+++ frontend/navigators/HomeNavigator.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { addNavigationHelpers, TabNavigator } from 'react-navigation';
+import { addNavigationHelpers, DrawerNavigator } from 'react-navigation';
 import UserProfile from '../components/profile/UserProfile';
 import MapItem from '../components/home/MapItem';
-// import Splash from '../components/Splash';
 
 const routeConfig = {
   MapItem: { screen: MapItem },
@@ -14,7 +13,7 @@ const homeNavigatorConfig = {
   initialRouteName: 'MapItem'
 };
 
-export const HomeNavigator = TabNavigator(
+export const HomeNavigator = DrawerNavigator(
   routeConfig,
   homeNavigatorConfig
 );
