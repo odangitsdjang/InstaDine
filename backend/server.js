d0909d61e55ce408699df732696fe9061c817c2b
Chu Jang <david.c.jang@gmail.com>
2017-10-11 22:42:45 -0700

    Working Heroku

--- backend/server.js
+++ backend/server.js
@@ -16,6 +16,7 @@ let router = require('./services/router');
 const devLink = 'mongodb://localhost:instaDine/instaDine';
 const productionLink = 'mongodb://heroku_xnlgzztq:2q53no3t91p0b1ep4uqln59dvh@ds117615.mlab.com:17615/heroku_xnlgzztq';
 
+//  if you only want to use localhost use:       mongoose.connect(devLink);
 mongoose.connect(productionLink || devLink);
 
 app.use(morgan('combined'));
@@ -26,4 +27,6 @@ let PORT = process.env.PORT || 3000;
 let HOST = process.env.HOST || '127.0.0.1';
 
 console.log('Listening on', HOST, PORT);
+// might need to change this line to the following while working locally
+// app.listen(PORT, HOST)
 app.listen(PORT);
