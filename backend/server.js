5aa1aae22f88bf70681ed446a1d91e44475e537e
Chu Jang <david.c.jang@gmail.com>
2017-10-11 22:29:37 -0700

    more heroku testing

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,10 @@ let router = require('./services/router');
 //   mongoose.connect('mongodb://localhost:user/user');
 // }
 
-mongoose.connect('mongodb://localhost:instaDine/instaDine');
+const devLink = 'mongodb://localhost:instaDine/instaDine';
+const productionLink = 'mongodb://heroku_xnlgzztq:2q53no3t91p0b1ep4uqln59dvh@ds117615.mlab.com:17615/heroku_xnlgzztq';
+
+mongoose.connect(productionLink || devLink);
 
 app.use(morgan('combined'));
 app.use(bodyParser.json());
@@ -23,4 +26,4 @@ let PORT = process.env.PORT || 3000;
 let HOST = process.env.HOST || '127.0.0.1';
 
 console.log('Listening on', HOST, PORT);
-app.listen(PORT, HOST);
+app.listen(PORT);
