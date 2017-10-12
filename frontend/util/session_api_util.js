fbabe9d275cf4aab76b6674d850d06c8371ff414
Chu Jang <david.c.jang@gmail.com>
2017-10-12 10:11:37 -0700

    Fix merge

--- frontend/util/session_api_util.js
+++ frontend/util/session_api_util.js
@@ -2,5 +2,5 @@
 export const productionUrl = `127.0.0.1:${process.env.PORT}`;
 let API_URL = 'http://localhost:3000/v1';
 
-exports.SIGNUP_URL = `/signup`;
+exports.SIGNUP_URL = `${API_URL}/signup`;
 exports.LOGIN_URL = `${API_URL}/login`;
