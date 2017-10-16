e52ee479cee6b99a36632b54286bef149a5c4b9b
addriv <adrivero89@gmail.com>
2017-10-15 23:36:39 -0700

    Update drawer styling

--- frontend/components/profile/DrawerContent.js
+++ frontend/components/profile/DrawerContent.js
@@ -67,7 +67,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-<<<<<<< HEAD
+    alignItems: 'center',
     backgroundColor: 'white',
     padding: 0
   },
@@ -112,11 +112,7 @@ const styles = StyleSheet.create({
   drawerItems: {
     borderWidth: 1,
     borderColor: 'black'
-=======
-    alignItems: 'center',
-    backgroundColor: '#4C5B61',
->>>>>>> dcd573389af4f2746cc2270a1860a0dab8f84f7b
-  },
+  }
 });
 
 export default DrawerContent;
